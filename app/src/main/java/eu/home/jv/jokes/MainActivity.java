2318eb86e7af50a2ae820f6fc52f1083e54afd50
Jury <54391036+Jury30094@users.noreply.github.com>
2023-04-26 22:45:43 +0300

    Initial Jokes project

---
+++ app/src/main/java/eu/home/jv/jokes/MainActivity.java
@@ -0,0 +1,14 @@
+package eu.home.jv.jokes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
